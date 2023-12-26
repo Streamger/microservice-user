d8478e8655bac7da487802bf35ee852b4ac348dd
Roshan-Ojha <roshanojha65@gmail.com>
2023-12-26 12:31:36 +0545

    managed structured of returned data

--- src/core/settings.py
+++ src/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -56,7 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True # For testing purposes; restrict it in production
 
 ROOT_URLCONF = 'core.urls'
 
